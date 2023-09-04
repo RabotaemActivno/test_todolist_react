cffed376ff412f86f4fb645b6785d282cd8624d7
RabotaemActivno <fajzovigor5@gmail.com>
2023-09-04 15:22:47 +0300

    end the modal functional

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,22 @@ import Item from "./components/Item/index";
 import { useState, useEffect } from "react";
 import Modal from "./components/Modal";
 
+type InputType = {
+  title: string;
+  description: string;
+  date: number
+}
+
 function App() {
   const [activeModal, setActiveModal] = useState<boolean>(false)
   const [data, setData] = useState<any[]>([]);
   const [quantity, setQuantity] = useState<number>(0);
+  const [input, setInput] = useState<InputType>({
+    title: '',
+    description: '',
+    date: 0,
+  })
+
   useEffect(() => {
     axios
       .get(`https://jsonplaceholder.typicode.com/todos`)
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,31 +1,84 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import styles from "./modal.module.scss";
 
 type Props = {
-    activeModal: boolean;
-    setActiveModal: (flag: boolean) => void
-}
+  activeModal: boolean;
+  setActiveModal: (flag: boolean) => void;
+};
+
+type InputType = {
+  title: string;
+  description: string;
+  date: string;
+};
+
+const Modal = ({ activeModal, setActiveModal }: Props) => {
+  
+  const [errorField, setErrorField] = useState<boolean>(false);
+  const [input, setInput] = useState<InputType>({
+    title: "",
+    description: "",
+    date: "",
+  });
 
 
-const Modal = ({activeModal, setActiveModal}: Props) => {
+  const onCancelClickHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    setInput({ title: "", description: "", date: "" });
+    setActiveModal(false);
+    setErrorField(false)
+  };
 
-    const onCancelClickHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>)=> {
-        e.preventDefault()
-        setActiveModal(false)
+  const onChangeInputHandler = (
+    e: ChangeEvent<HTMLInputElement>,
+    fieldName: string
+  ) => {
+    setErrorField(false);
+    setInput((prevState) => ({
+      ...prevState,
+      [fieldName]: e.target.value,
+    }));
+  };
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input.title && input.description && input.date) {
+      console.log(input);
+      setActiveModal(false);
+    } else {
+      setErrorField(true);
     }
+  };
 
   return (
-    <div className={activeModal ? styles.modal : styles.modal_disabled}>
+    <div className={activeModal ? (styles.modal) : styles.modal_disabled}>
       <div className={styles.wrapper}>
         <div className={styles.content}>
-          <form className={styles.form}>
-            <input type="text" placeholder="title" />
-            <input type="text" placeholder="description" />
-            <input className={styles.input_date} type="datetime-local" />
+          <form className={styles.form} onSubmit={(e) => onSubmit(e)}>
+            <input
+              type="text"
+              placeholder="title"
+              value={input.title}
+              onChange={(e) => onChangeInputHandler(e, "title")}
+            />
+            <input
+              type="text"
+              placeholder="description"
+              value={input.description}
+              onChange={(e) => onChangeInputHandler(e, "description")}
+            />
+            <input
+              className={styles.input_date}
+              type="datetime-local"
+              value={input.date}
+              onChange={(e) => onChangeInputHandler(e, "date")}
+            />
             <div className={styles.button_wrapper}>
               <button onClick={onCancelClickHandler}>Cancel</button>
               <button type="submit">Add</button>
             </div>
+            {errorField && <div className={styles.errorMessage}>Fill in all the fields</div>}
           </form>
         </div>
       </div>
